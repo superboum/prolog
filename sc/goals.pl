a749992240fb8580889752e04512617c330f6d66
Quentin Dufour <quentin@dufour.io>
2018-12-09 13:52:58 +0100

    New way to filter

--- sc/goals.pl
+++ sc/goals.pl
@@ -8,13 +8,13 @@ mostProfitableTrip(Departure, Arrival, Merchandise, CanBuy, SCU, MaxUEC, Profit)
   maximize(Profit),
   \+ notMostProfitableTrip(SCU, MaxUEC, Profit).
 
-% @FIXME: wrong result
+notMostProfitableTwoWayTrip(SCU, MaxUEC, Profit) :-
+  twoWayTrip(_,_,_,_,_,_,SCU,MaxUEC,_,_,ConcProfit),
+  maximize(ConcProfit),
+  Profit < ConcProfit.
+
 mostProfitableTwoWayTrip(Departure, Arrival, Merchandise1, Merchandise2, CanBuy1, CanBuy2, SCU, MaxUEC, Profit1, Profit2, FinalProfit) :-
   twoWayTrip(Departure, Arrival, Merchandise1, Merchandise2, CanBuy1, CanBuy2, SCU, MaxUEC, Profit1, Profit2, FinalProfit),
-  twoWayTrip(ConcD, ConcA, ConcM1, ConcM2, _, _, SCU, MaxUEC, ConcP1, ConcP2, ConcFP),
-  (Departure \= ConcD ; Arrival \= ConcA ; Merchandise1 \= ConcM1 ; Merchandise2 \= ConcM2),     
-  maximize(Profit1), maximize(Profit2),                                                            
-  maximize(ConcP1), maximize(ConcP2),                                           
-  { FinalProfit > ConcFP },
-  !.                                                                              
+  maximize(FinalProfit),
+  \+ notMostProfitableTwoWayTrip(SCU, MaxUEC, FinalProfit).
 
