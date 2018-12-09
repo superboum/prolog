66ad8837a0b9b013792d9eee058e396a0464140b
Quentin Dufour <quentin@dufour.io>
2018-12-09 12:34:12 +0100

    We might have found the solution

--- sc/rules.pl
+++ sc/rules.pl
@@ -17,19 +17,31 @@ expectableProfit(Bought, CostBuy, CostSell, Profit) :-
     Profit = Bought * (CostSell - CostBuy)
   }.
 
-journey(Departure, Arrival, Merchandise, CanBuy, SCU, MaxUEC, Profit) :-
+trip(Departure, Arrival, Merchandise, CanBuy, SCU, MaxUEC, Profit) :-
   buy(Departure, Merchandise, CostBuy),
   sell(Arrival, Merchandise, CostSell),
   buyableCargo(CostBuy, SCU, MaxUEC, CanBuy),
   expectableProfit(CanBuy, CostBuy, CostSell, Profit).
 
+trip2(Departure, Arrival, Merchandise, CanBuy, SCU, MaxUEC, Profit) :-
+  trip(Departure, Arrival, Merchandise, CanBuy, SCU, MaxUEC, Profit),
+  trip(ConcDep, ConcArrival, ConcMerch, _, SCU, MaxUEC, ConcProfit),
+  (Departure \= ConcDep ; Arrival \= ConcArrival ; Merchandise \= ConcMerch),
+  { Profit > ConcProfit }.
+
 twoWayTrip(Departure, Arrival, Merchandise1, Merchandise2, CanBuy1, CanBuy2, SCU, MaxUEC, Profit) :-
-  journey(Departure, Arrival, Merchandise1, CanBuy1, SCU, MaxUEC, Profit1),
-  journey(Arrival, Departure, Merchandise2, CanBuy2, SCU, MaxUEC + Profit1, Profit2),
+  trip(Departure, Arrival, Merchandise1, CanBuy1, SCU, MaxUEC, Profit1),
+  trip(Arrival, Departure, Merchandise2, CanBuy2, SCU, MaxUEC + Profit1, Profit2),
   {
     Profit = Profit1 + Profit2
   }.
 
 mostProfitableTwoWayTrip(SCU, MaxUEC, Sorted) :-
-  findall((P,D,A,M1,M2,C1,C2), (twoWayTrip(D,A,M1,M2,C1,C2,SCU,MaxUEC,P), safe(D,A)), Res),
+  findall(
+    (Profit,Departure,Arrival,Merch1,Merch2,Count1,Count2),
+    (
+      twoWayTrip(Departure,Arrival,Merch1,Merch2,Count1,Count2,SCU,MaxUEC,Profit),
+      safe(Departure,Arrival),
+      maximize(Profit)
+    ), Res),
   sort(0, @>=, Res, Sorted).
